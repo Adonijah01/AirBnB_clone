31bc116494684f0577fe3e35d88e78b040daafc9
adonijah01 <adonijahsingoei@gmail.com>
2023-08-14 10:47:27 +0300

    Airbnb

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Testing for storage
+"""
+from datetime import datetime
+import unittest
+from time import sleep
+import json
+from models.engine.file_storage import FileStorage
+
+
+class test_fileStorage(unittest.TestCase):
+    """Testing fileStorage Class"""
+    def test_instances(self):
+        """cheque  instantation"""
+        obj = FileStorage()
+        self.assertIsInstance(obj, FileStorage)
+
+    def test_docs(self):
+        """test doc strings"""
+        self.assertIsNotNone(FileStorage.all)
+        self.assertIsNotNone(FileStorage.new)
+        self.assertIsNotNone(FileStorage.save)
+        self.assertIsNotNone(FileStorage.reload)
+
+    if __name__ == '__main__':
+        unittest.main()
